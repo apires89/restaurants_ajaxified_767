f5021e4415d47c49903b179dd9270eb6241a10d4
andre <andrecabralpires@gmail.com>
2021-11-25 18:19:26 +0000

    first push

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
   root to: 'pages#home'
+    resources :restaurants, only: [ :index, :show ] do
+    resources :reviews, only: :create
+    end
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,19 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+# db/seeds.rb
+puts 'Creating restaurants...'
+Restaurant.create!({
+  name: "Quik Asia",
+  address: "Lisbon"
+})
+Restaurant.create!({
+  name: "El Pibe",
+  address: "Lisbon"
+}
+Restaurant.create!({
+  name: "Forninho",
+  address: "Lisbon"
+})
+Restaurant.create!({
+  name: "Luzzo",
+  address: "Lisbon"
+})
+puts 'Finished!'
